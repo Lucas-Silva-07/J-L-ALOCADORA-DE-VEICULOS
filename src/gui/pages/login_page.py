cf821f9dc7430fc263c438660d322e244bad9586
Lucas <lucasalves_07@outlook.com>
2025-10-24 12:01:53 -0300

    Mudando o login_page.py Agora usando base_page.py para criar janela Usando herança e polimorfismo de uma classe pai Deixando o codigo mais legivel e modular

--- src/gui/pages/login_page.py
+++ src/gui/pages/login_page.py
@@ -1,89 +1,101 @@
-from src.config import ICO_PATH, LOGO_PATH, BOTAO_LOGIN, BOTAO_LOGIN_HOVER, EYE_PATH, CLOSE_EYE_PATH, FONT_POPINS
+from src.gui.pages.base_page import BasePage
+from src.config import EYE_PATH, CLOSE_EYE_PATH
 from PIL import Image
 import customtkinter
 
 
-class LoginPage(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-        #Configurações da janela
-        self.title("JL Alocadora de Veículos")
-        self.largura_janela = 930
-        self.altura_janela = 530
-        self.largura_tela = self.winfo_screenwidth()
-        self.altura_tela = self.winfo_screenheight()
-        x = (self.largura_tela / 2) - (self.largura_janela / 2)
-        y = (self.altura_tela / 2) - (self.altura_janela / 2)
-        self.geometry(f"{self.largura_janela}x{self.altura_janela}+{int(x)}+{int(y)}")
-        self.grid_columnconfigure(0, weight=0)
-        self.grid_rowconfigure((0, 1), weight=0)
-        self.configure(fg_color="#CECECE")
-        self.iconbitmap(ICO_PATH)
-        self.resizable(False, False)
-        
-        #Criando Frame login
-        self.checkbox_frame = customtkinter.CTkFrame(self)
-        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky='nw')        
-        self.checkbox_frame.configure(fg_color="#CECECE")
-        #Fonte personalizada
-        customtkinter.FontManager.load_font(FONT_POPINS)
-        #Label Bem Vindo
-        self.texto1 = customtkinter.CTkLabel(self.checkbox_frame, text="ACESSE SUA CONTA", font=("Poppins SemiBold", 30, "bold"), text_color="black")
-        self.texto1.grid(row=0, column=0, padx=75, pady=(75, 0), sticky='w')
-        #Label pedindo email 
-        self.texto2 = customtkinter.CTkLabel(self.checkbox_frame, text="Por favor, insira seus dados.", font=("Poppins SemiBold", 14), text_color="#625B5B")
-        self.texto2.grid(row=1, column=0, padx=75, pady=(0, 0), sticky='w')
-        #Label email
-        self.texto3 = customtkinter.CTkLabel(self.checkbox_frame, text="Email", font=("Poppins SemiBold", 16), text_color="black")
-        self.texto3.grid(row=2, column=0, padx=75, pady=(20, 0), sticky='w')
-        #Caixa entrada email
-        self.entry_email = customtkinter.CTkEntry(self.checkbox_frame, text_color= "black", placeholder_text="Digite seu email", fg_color="#CECECE", border_color="#F1C0C0", width=300, height=40, font=("Poppins SemiBold", 12))
-        self.entry_email.grid(row=3, column=0, padx=75, pady=(0, 0), sticky='w')
-        #Label Senha
-        self.texto4 = customtkinter.CTkLabel(self.checkbox_frame, text="Senha", font=("Poppins SemiBold", 16), text_color="black")
-        self.texto4.grid(row=4, column=0, padx=75, pady=(20, 0), sticky='w')       
-        #Caixa entrada senha
-        self.entry_senha = customtkinter.CTkEntry(self.checkbox_frame, text_color= "black", show="*", placeholder_text="Digite a senha", fg_color="#CECECE", border_color="#ADADAD", width=300, height=40, font=("Poppins SemiBold", 12))
-        self.entry_senha.grid(row=5, column=0, padx=75, pady=(0, 0), sticky='w')
-        # Botão do olhinho ao lado do entry
-        self.eye_open_img = customtkinter.CTkImage(Image.open(EYE_PATH), size=(35, 15))
-        self.eye_closed_img = customtkinter.CTkImage(Image.open(CLOSE_EYE_PATH), size=(35, 15))
-        self.btn_eye = customtkinter.CTkButton(self.checkbox_frame, text="", image=self.eye_closed_img, command=self.alternar_password, fg_color="transparent", width=5, hover=None)
-        self.btn_eye.place(x=320, y=300)  # ajusta posição conforme necessário
-
-        #Botão entrar
-        #Imagem botão normal
-        self.img_normal = customtkinter.CTkImage(
-            light_image=Image.open(BOTAO_LOGIN),
-            dark_image=Image.open(BOTAO_LOGIN),
-            size=(300, 40)
+class LoginPage(BasePage):
+    def __init__(self, controller=None):
+        super().__init__(controller)
+        self.configurar_janela()
+        self.carregar_recursos()
+        self.criar_frame_principal()
+        self.criar_logo()
+    
+    # ============================================
+    # CRIANDO AS LABELS DA JANELA
+    # ============================================
+        self.criar_label(
+            "ACESSE SUA CONTA", row=0, pady=(75, 0), font_size=30, weight="bold"
         )
-        #Imagem quando ficar com mouse em cima
-        self.img_hover = customtkinter.CTkImage(
-            light_image=Image.open(BOTAO_LOGIN_HOVER),
-            dark_image=Image.open(BOTAO_LOGIN_HOVER),
-            size=(300, 40)
-        )
-        self.button = customtkinter.CTkButton(self.checkbox_frame, text="",
-                                                    command=self.button_callback,
-                                                    width=300, 
-                                                    height=40, 
-                                                    fg_color="transparent", 
-                                                    image=self.img_normal,
-                                                    hover_color= "#CECECE")
-        self.button.grid(row=6, column=0, padx=10, pady=(20, 0))
-        self.button.bind("<Enter>", lambda e: self.button.configure(image = self.img_hover))
-        self.button.bind("<Leave>", lambda e: self.button.configure(image = self.img_normal))
-        #Cria uma imagem simples 
-        self.image_logo = customtkinter.CTkImage(Image.open(LOGO_PATH), size=(530, 530))
-        #Exibe a imagem em um Label
-        self.logo_label = customtkinter.CTkLabel(self, image=self.image_logo, text="")
-        self.logo_label.place(x=400, y=-20) 
 
+        self.criar_label(
+            "Por favor, insira seus dados.", row=1, font_size=14, color="#625B5B"
+        )
+
+        self.criar_label(
+            "Email:", row=2, pady=(20, 0), font_size=16
+        )
+
+        self.criar_label(
+            "Senha:", row=4, pady=(20, 0), font_size=16
+        )
+    
+    # ============================================
+    # CRIANDO OS ENTRY DA JANELA
+    # ============================================
+        self.entry_email = self.criar_entry(
+            placeholder="Digite seu email",
+            row=3     
+        )
         
+        self.entry_senha = self.criar_entry(
+            placeholder="Digite a senha",
+            row=5,
+            show="*"
+        )
+
+        self.btn_eye = customtkinter.CTkButton(
+            self.frame_menu,
+            text="", 
+            image=self.eye_closed_img, 
+            command=self.alternar_password, fg_color="transparent", 
+            width=5, 
+            hover=None
+            )
+        
+        self.btn_eye.place(x=320, y=300)
+    # ============================================
+    # CRIANDO BOTÃO ENTRAR
+    # ============================================
+        self.criar_botoes([
+            ("ENTRAR", self.btn_entrar, 6, 0, 75, (20, 0))
+        ])
+
+
+    #  FUNÇÃO PARA CRIAR OS ENTRY
+    def criar_entry(self, placeholder="", row=0, column=0, padx=75, pady=(0, 0), sticky='w', show=""):
+        entry = customtkinter.CTkEntry(
+            self.frame_menu, 
+            text_color= "gray", 
+            placeholder_text=placeholder, 
+            fg_color="#CECECE", 
+            border_color="#A3A3A3", 
+            width=300, 
+            height=40, 
+            font=("Poppins SemiBold", 14),
+            show=show
+            )
+            
+        entry.grid(row=row, column=column, padx=padx, pady=pady, sticky=sticky)
+        return entry
+
+
     #Função do botão entrar
-    def button_callback(self):
-        print("button pressed")
+    def btn_entrar(self):
+        print("Entrar pressionado")
+
+    def carregar_recursos(self):
+        super().carregar_recursos()
+        self.eye_open_img = customtkinter.CTkImage(
+            Image.open(EYE_PATH),
+            size=(35, 15)
+        )
+        self.eye_closed_img = customtkinter.CTkImage(
+            Image.open(CLOSE_EYE_PATH),
+            size=(35, 15)
+        )
+
     #Função senha mostrar/ocultar 
     def alternar_password(self):
         if self.entry_senha.cget("show") == "":
@@ -92,6 +104,8 @@ class LoginPage(customtkinter.CTk):
         else:
             self.entry_senha.configure(show="")
             self.btn_eye.configure(image=self.eye_open_img)  # olho aberto
+
+
 
 app = LoginPage()
 app.mainloop()
