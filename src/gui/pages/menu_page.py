7cfc901dd0c9a57d4f09c770ebe76a619c0ae3e0
Lucas <lucasalves_07@outlook.com>
2025-10-26 08:35:51 -0300

    Adaptando a pagina de menu para usar em frames empilhados dentro de um container

--- src/gui/pages/menu_page.py
+++ src/gui/pages/menu_page.py
@@ -2,18 +2,23 @@ from src.gui.pages.base_page import BasePage
 
 
 class MenuPage(BasePage):
-    def __init__(self, controller):
-        super().__init__(controller)
-        self.configurar_janela()
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
         self.carregar_recursos()
         self.criar_frame_principal()
         self.criar_logo()
+    # ============================================
+    # CRIANDO BOTÕES DA TELA
+    # ============================================
         self.criar_botoes([
             ("Nova Consulta", self.nova_consulta, 0, 0, 75, (140, 0)),
             ("Carros Alugados", self.carros_alugados, 1, 0, 75, (20, 0)),
             ("Configuração de Conta", self.configuracao_conta, 2, 0, 75, (20, 0)),
             ("Sair da Conta", lambda: self.trocar_tela("LoginPage"), 3, 0, 75, (20, 0))
         ])
+    # ============================================
+    # TEXTO PRINCIPAL
+    # ============================================
         self.criar_label(
             "MENU PRINCIPAL", 
             font_size=34,
@@ -22,7 +27,7 @@ class MenuPage(BasePage):
             x=320,
             y=40,
             parent=self
-            )
+        )
     # ============================================
     # FUNÇÕES DOS BOTÕES
     # ============================================
@@ -34,9 +39,6 @@ class MenuPage(BasePage):
 
     def configuracao_conta(self):
         print("Configuração de conta pressionado")
-
-    def sair_conta(self):
-        print("Sair da conta pressionado")
 
 
 if __name__ == "__main__":
